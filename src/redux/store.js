c4f933581e00cba493d4dffd600c5b08ba82308e
Pablo <ezekielgonzalezr@gmail.com>
2023-06-26 21:36:46 -0600

    Configure store

---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import airqaReducer from "./airqaSlice";
+const store = configureStore({
+    reducer: {
+        airqa: airqaReducer,
+    },
+});
+export default store;
