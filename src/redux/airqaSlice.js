f3ed38beee8880fc4c2e5db8c4a50b8f6b1f70af
Pablo <ezekielgonzalezr@gmail.com>
2023-06-27 21:51:52 -0600

    Use local storage

--- src/redux/airqaSlice.js
+++ src/redux/airqaSlice.js
@@ -2,26 +2,41 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { APIURL } from '../api/url';
 
+const citiesData = [{
+  name: 'Paris',
+  lat: 48.8588897,
+  lon: 2.3200410217200766,
+  country: 'FR',
+  state: 'Ile-de-France',
+}, {
+  name: 'New York County',
+  lat: 40.7127281,
+  lon: -74.0060152,
+  country: 'US',
+  state: 'New York',
+}, {
+  name: 'Worcester',
+  lat: 51.4875167,
+  lon: -0.1687007,
+  country: 'GB',
+  state: 'England',
+}, {
+  name: 'Dharuhera',
+  lat: 28.2055216,
+  lon: 76.7953099,
+  country: 'IN',
+  state: 'Haryana',
+},
+];
 const initialState = {
-  airdata: [],
-  cities: [{
-    lat: 48.8588897,
-    lon: 2.3200410217200766,
-    country: 'FR',
-    state: 'Ile-de-France',
-  }, {
-    lat: 40.7127281,
-    lon: -74.0060152,
-    country: 'US',
-    state: 'New York',
-  },
-  ],
+  airdata: JSON.parse(localStorage.getItem('airdataAPI')) || [],
+  cities: citiesData,
   isLoading: false,
   error: undefined,
 };
 
-export const getData = createAsyncThunk('airqa/getData', async () => {
-  const promises = initialState.cities.map(async (ele) => {
+export const getData = createAsyncThunk('airqa/getData', async (array) => {
+  const promises = array.map(async (ele) => {
     const url = APIURL(ele.lat, ele.lon);
     try {
       const resp = await axios(url);
@@ -49,8 +64,8 @@ const airqaSlice = createSlice({
       })
       .addCase(getData.fulfilled, (state, { payload }) => {
         state.isLoading = false;
-        // console.log(payload);
         state.airdata = payload;
+        localStorage.setItem('airdataAPI', JSON.stringify(state.airdata));
       })
       .addCase(getData.rejected, (state) => {
         state.error = true;
