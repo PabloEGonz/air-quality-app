c4f9c6942e65f818064c4531f4b601761037c336
Pablo <ezekielgonzalezr@gmail.com>
2023-06-28 12:38:15 -0600

    Add details page

--- src/pages/details.js
+++ src/pages/details.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import CardDetails from '../components/cardDetls';
 
-const Details = () => (
-  <div>Details</div>
-);
+const Details = () => {
+  const { airdata } = useSelector((state) => state.airqa);
+
+  return (
+    <ul className="card-container-details">
+      {airdata.map((ele) => (
+        <CardDetails key={ele.lon} data={ele} />
+      ))}
+    </ul>
+  );
+};
 
 export default Details;
