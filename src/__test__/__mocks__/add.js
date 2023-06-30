cb6a2b61672b3cdd15bf0045f8c45f4de3fa5751
Pablo <ezekielgonzalezr@gmail.com>
2023-06-30 06:47:57 -0600

    Create tests for add component

---
+++ src/__test__/__mocks__/add.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Add = () => (
+  <>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <h3 className="navbar-brand">Air Quality App</h3>
+      </div>
+    </nav>
+    <h1 className="under-dev">Coming soon</h1>
+  </>
+);
+
+export default Add;
