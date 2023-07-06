518a98a03b4736927bd951f86de767fc1f57be1a
Pablo <ezekielgonzalezr@gmail.com>
2023-07-06 14:59:20 -0600

    Improve cities section desktop version

--- src/pages/details.js
+++ src/pages/details.js
@@ -13,7 +13,7 @@ const Details = () => {
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <Link className="return" to="/"><BsFillArrowLeftSquareFill className="option" /></Link>
+          <Link className="return" to="/cities"><BsFillArrowLeftSquareFill className="option" /></Link>
           <h3 className="navbar-brand">Air Quality App</h3>
         </div>
       </nav>
