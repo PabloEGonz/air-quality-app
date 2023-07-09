2cdf98de28281feacc8da370aa5795e33ed5ba96
Pablo <ezekielgonzalezr@gmail.com>
2023-07-09 12:31:02 -0600

    Add link to cities

--- src/pages/remove.jsx
+++ src/pages/remove.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../styles/add.css';
 import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { v4 as uuidV4 } from 'uuid';
 import { BsFillArrowLeftSquareFill } from 'react-icons/bs';
 import Locations from '../components/locations';
@@ -11,19 +11,20 @@ const Remove = () => {
   const { cities } = useSelector((state) => state.airqa);
 
   return (
-    <>
+    <div className="remove">
       <nav className="navbar">
         <BsFillArrowLeftSquareFill className="return" onClick={() => navigate(-1)} />
         <h3 className="brand">Air Quality App</h3>
       </nav>
       <div>
+        <Link to="/cities" style={{ textDecoration: 'none' }}><button type="button" className="access">See Cities</button></Link>
         <ul className="card-loc-container">
           {cities.map((el) => (
             <Locations key={uuidV4()} info={el} action="delete" />
           ))}
         </ul>
       </div>
-    </>
+    </div>
   );
 };
 
