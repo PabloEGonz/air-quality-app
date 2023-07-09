99ef34ac7b7dd2caf5071a89b235e3d6f4bee99b
Pablo <ezekielgonzalezr@gmail.com>
2023-07-09 16:00:09 -0600

    Add lead paragraph and use balancer

--- src/pages/options.js
+++ src/pages/options.js
@@ -5,6 +5,7 @@ import { MdLibraryAdd } from 'react-icons/md';
 import { TbListDetails } from 'react-icons/tb';
 import { BsPatchQuestionFill } from 'react-icons/bs';
 import { RiAccountCircleFill } from 'react-icons/ri';
+import Balancer from 'react-wrap-balancer';
 
 const Options = () => (
   <>
@@ -12,6 +13,14 @@ const Options = () => (
       <Link to="/"><RiAccountCircleFill className="account" /></Link>
       <h3 className="brand">Air Quality App</h3>
     </nav>
+    <p className="lead">
+      <Balancer>
+        This web app provides users with an easy way to monitor
+        air quality in different cities.
+        Users can view air quality information for each city, add new cities, remove cities,
+        and learn about the parameters used to determine air quality levels.
+      </Balancer>
+    </p>
     <div className="options">
       <Link className="link" to="/cities">
         <FaMapMarked className="option" />
